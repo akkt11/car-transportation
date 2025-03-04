e65c2c07b44c3a799e8ccb39596ef7362b6d5da8
akktii <aktan.m@reviro.io>
2025-03-04 10:52:56 +0600

    temporary commit for deploy

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -35,8 +35,8 @@ const config: Config = {
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
         moveText: {
-          "0%": { transform: "translateX(-100%)" },
-          "100%": { transform: "translateX(0)" },
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
         },
       },
     },
